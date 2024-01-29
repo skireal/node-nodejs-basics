732055f0c906cf4bbef34c0cda11ed36b1708d3a
Kirill Shishov <skireal@mail.ru>
2024-01-29 23:51:52 +0300

    feat: implement stream read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,27 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+  const readableStream = fs.createReadStream(filePath);
+
+  return new Promise((resolve, reject) => {
+    readableStream.on('data', (chunk) => {
+      process.stdout.write(chunk);
+    });
+
+    readableStream.on('end', () => {
+      resolve();
+    });
+
+    readableStream.on('error', (err) => {
+      reject(err);
+    });
+  });
 };
 
 await read();
