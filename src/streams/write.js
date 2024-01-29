f2c9988979a1e5a6b4e1fc4d29a93b4ee85d546d
Kirill Shishov <skireal@mail.ru>
2024-01-29 23:57:44 +0300

    feat: implement stream write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,25 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+  const writableStream = fs.createWriteStream(filePath);
+
+  process.stdin.pipe(writableStream);
+
+  return new Promise((resolve, reject) => {
+    writableStream.on('finish', () => {
+      resolve();
+    });
+
+    writableStream.on('error', (err) => {
+      reject(err);
+    });
+  });
 };
 
 await write();
